715da978231e69c3f35562e94be85805c3316b7c
unknown <pinlac.johncarlo@gmail.com>
2023-04-11 15:13:05 +0800

    Angular component lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature',
+      content: 'I saw this mountain today.',
+    },
+    {
+      title: 'Mountain Bike',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'nature',
+      content: 'I saw this bike today.',
+    },
+  ];
 }
